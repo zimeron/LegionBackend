8332e67d36bbfecabc68e1296da1221a3c90beac
Andrew Fisher <fishera11@xavier.edu>
2019-10-21 15:55:58 -0400

    Rails 6 syntax change?

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module LegionBackend
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins 'elderscrollslegion.herokuapp.com/*'
         resource(
