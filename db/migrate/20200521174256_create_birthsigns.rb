17df53b5f36b1ac598b2f9c76fe0d310a5129931
Andrew Fisher <fishera11@xavier.edu>
2020-05-21 13:59:33 -0400

    fixed integer type

--- db/migrate/20200521174256_create_birthsigns.rb
+++ db/migrate/20200521174256_create_birthsigns.rb
@@ -2,7 +2,7 @@ class CreateBirthsigns < ActiveRecord::Migration[6.0]
   def change
     create_table :birthsigns do |t|
       t.string :name, null: false
-      t.int :abilitymodifiers, array: true, default: [0, 0, 0, 0, 0, 0]
+      t.integer :abilitymodifiers, array: true, default: [0, 0, 0, 0, 0, 0]
       t.string :features, array: true, default: []
 
       t.timestamps
