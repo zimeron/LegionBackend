4bf111d3ff8a7db9808e8854e5c7a297cdd0ee03
Andrew Fisher <fishera11@xavier.edu>
2020-06-27 14:22:35 -0400

    forgot the route again

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :backgrounds
   resources :races
   resources :birthsigns
+  resources :characterclasses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
