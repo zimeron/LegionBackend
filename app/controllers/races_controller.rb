75cacbec22fa4ab4e0c462dddb196be6c76986d6
Andrew Fisher <fishera11@xavier.edu>
2020-10-30 23:06:31 -0400

    hopefully this will pull subrace and splice into race correctly, probably a more elegeant way to join

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -5,6 +5,21 @@ class RacesController < ApplicationController
     
     def show
         @race = Race.find(params[:id])
+        i = 0
+        if @race.hassubrace
+            @subrace = SubRace.where(race: @race.name)
+                              .order(Arel.sql('RANDOM()'))
+                              .first
+            puts @subrace
+            @race.features.push + @subrace.features
+            @subrace.abilitymodifiers do |abilitymod|
+                @race.abilitymodifiers[i] += abilitymod
+                i += 1
+            @race.size = @subrace.size
+            @race.skillproficiencies + @subrace.skillproficiencies
+            @race.toolsandlanguages + @subrace.toolsandlanguages
+            @race.name = @subrace.name + @race.name
+        end
         puts @race.to_json
         render json: @race.to_json
     end
